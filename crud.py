5e7154a9663e1c3cb99cc45ed4b4a96a7700cba4
Mobel_Coding <azizbeknurmuhammadov747@gmail.com>
2026-03-26 20:15:14 +0500

    Refactor CRUD operations for categories in crud.py

--- crud.py
+++ crud.py
@@ -1,36 +1,55 @@
-from sqlalchemy.future import select
+
+
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from fastapi import HTTPException
+
+from schemas import (CategoryCreate, CategoryResponse)
 from models import Category, News
 
-
-# CATEGORY CRUD
-async def create_category(db: AsyncSession, name: str):
-    category = Category(name=name)
-    db.add(category)
+async def create_category(category: CategoryCreate, db: AsyncSession) -> CategoryResponse:
+    db_category = Category(**category.model_dump())
+    db.add(db_category)
     await db.commit()
-    await db.refresh(category)
-    return category
+    await db.refresh(db_category)
+    return CategoryResponse.model_validate(db_category)
 
 
-async def get_categories(db: AsyncSession):
-    result = await db.execute(select(Category))
-    return result.scalars().all()
+async def read_categories(db: AsyncSession) -> list[CategoryResponse]:
+    results = await db.execute(select(Category))
+    return [CategoryResponse.model_validate(category)for category in results.scalars().all()]
 
 
-async def get_category(db: AsyncSession, category_id: int):
-    result = await db.execute(select(Category).where(Category.id == category_id))
-    return result.scalar_one_or_none()
+async def read_category(category_id: int, db: AsyncSession) -> CategoryResponse:
+    category = await db.get(Category, category_id)
+    if not category:
+        raise HTTPException(status_code=404,detail='hato')
+    return CategoryResponse.model_validate(category)
 
 
-async def delete_category(db: AsyncSession, category_id: int):
-    category = await get_category(db, category_id)
-    if category:
-        await db.delete(category)
-        await db.commit()
-    return category
+async def update_category(category_id: int, category: CategoryCreate, db: AsyncSession) -> CategoryResponse:
+    db_category = await db.get(Category, category_id)
+    if not db_category:
+        raise HTTPException(status_code=404,detail='hato')
+    for attr, value in category.__dict__.items():
+        setattr(db_category, attr, value)
+
+    await db.commit()
+    await db.refresh(db_category)
+
+    return CategoryResponse.model_validate(db_category)
 
 
-# NEWS CRUD
+async def delete_category(category_id: int, db: AsyncSession) -> dict:
+    category = await db.get(Category, category_id)
+    if not category:
+        raise HTTPException(status_code=404,detail='hato')
+
+    await db.delete(category)
+    await db.commit()
+
+    return {'message': 'deleted'}
+
 async def create_news(db: AsyncSession, news_data):
     news = News(**news_data.dict())
     db.add(news)
@@ -55,3 +74,4 @@ async def delete_news(db: AsyncSession, news_id: int):
         await db.delete(news)
         await db.commit()
     return news
+
